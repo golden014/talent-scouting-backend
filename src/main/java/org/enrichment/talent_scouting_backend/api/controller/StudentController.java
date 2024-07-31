75147bec752b4e07a012c1cd2d55ae51937a62e0
H-A-L-F <arya.jayadi2013@gmail.com>
2024-07-31 14:32:17 +0700

    minor fix

--- src/main/java/org/enrichment/talent_scouting_backend/api/controller/StudentController.java
+++ src/main/java/org/enrichment/talent_scouting_backend/api/controller/StudentController.java
@@ -1,18 +1,10 @@
-<<<<<<<< HEAD:src/main/java/org/enrichment/talent_scouting_backend/controller/StudentController.java
-package org.enrichment.talent_scouting_backend.controller;
-
-import org.enrichment.talent_scouting_backend.model.Student;
-import org.enrichment.talent_scouting_backend.service.student.StudentService;
-========
 package org.enrichment.talent_scouting_backend.api.controller;
+import org.enrichment.talent_scouting_backend.service.student.StudentService;
 
 import org.enrichment.talent_scouting_backend.api.model.Student;
-import org.enrichment.talent_scouting_backend.service.StudentService;
->>>>>>>> admin:src/main/java/org/enrichment/talent_scouting_backend/api/controller/StudentController.java
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
